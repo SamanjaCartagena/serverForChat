8f654fcd665a00ebc1e9792a59f9fa0d4e5736b4
Samanja Cartagena <c.samanja09@Pabs.local>
2021-08-08 19:20:06 -0400

    trying to resolve

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import index from '../index';
 
-ReactDOM.render(<index />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
