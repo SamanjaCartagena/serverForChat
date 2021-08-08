3b284d2434391a9a1d255e1bb918189b7acccdd7
Samanja Cartagena <c.samanja09@Pabs.local>
2021-08-08 19:18:09 -0400

    index server is running

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import index from '../index';
+
+ReactDOM.render(<index />, document.getElementById('root'));
